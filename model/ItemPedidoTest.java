90bf58e57bfb24b511f3ce17911a63fcb086563e
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2026-04-24 14:39:14 -0300

    Teste para o Pedido, com o setup inicial
    Isso é para não ficar toda hora instanciando a classe Produto.

--- model/ItemPedidoTest.java
+++ model/ItemPedidoTest.java
@@ -1,1 +1,17 @@
+package model;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+class ItemPedidoTest {
+
+    private Produto produtoPadrao;
+
+    @BeforeEach
+    void setUp() {
+        // Preparação: Criamos um produto que será usado nos testes do Item
+        produtoPadrao = new Produto("Teclado Mecânico", 250.00);
+    }
+}
