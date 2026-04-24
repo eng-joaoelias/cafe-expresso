a53018e42ca1e0f41d4d696f53e498bbcc4dd824
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2026-04-24 15:02:02 -0300

    Add getters e setters para a classe Pedido.

--- model/Pedido.java
+++ model/Pedido.java
@@ -4,4 +4,18 @@ import java.util.ArrayList;
 
 public class Pedido {
 	private ArrayList <ItemPedido> itens;
+	private StatusPedido status;
+	
+	
+	public ArrayList<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public StatusPedido getStatus() {
+		return status;
+	}
+	public void setStatus(StatusPedido status) {
+		this.status = status;
+	}
+	
 }
