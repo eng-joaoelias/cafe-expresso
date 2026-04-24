c9e4d60d5d5176aac37ecee65d19c699cdbbae58
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2026-04-24 14:32:46 -0300

    Teste com criação de produto

--- model/ProdutoTest.java
+++ model/ProdutoTest.java
@@ -1,1 +1,17 @@
+package model;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+class ProdutoTest {
+
+    @Test
+    @DisplayName("Deve criar um produto corretamente com construtor")
+    void deveCriarProdutoComConstrutor() {
+        Produto produto = new Produto("Notebook", 3500.00);
+        
+        assertEquals("Notebook", produto.getNome());
+        assertEquals(3500.00, produto.getPrecounitario());
+    }
+}
