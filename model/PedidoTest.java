2ae4455fffcb28cc0f7d3c61098724f22c979953
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2026-04-24 15:29:24 -0300

    Teste para soma de pedidos.

--- model/PedidoTest.java
+++ model/PedidoTest.java
@@ -27,4 +27,18 @@ public class PedidoTest {
         assertEquals(1, pedido.getItens().size(), "A lista de itens deve conter exatamente 1 item");
         assertEquals("Café Expresso", pedido.getItens().get(0).getProduto().getNome());
     }
+
+	@Test
+    @DisplayName("Deve calcular o valor total corretamente para múltiplos itens")
+    void deveCalcularTotalCorretamente() {
+        // 2 Cafés (2 * 5.00 = 10.00)
+        pedido.adicionarItem(cafe, 2);
+        // 3 Pães de Queijo (3 * 4.50 = 13.50)
+        pedido.adicionarItem(paoDeQueijo, 3);
+
+        double totalEsperado = 23.50;
+        double totalObtido = pedido.calcularTotal();
+
+        assertEquals(totalEsperado, totalObtido, 0.001, "O cálculo do total deve ser a soma dos subprodutos"); //O terceiro valor é para garantir que minimas desigualdades decorrentes do compilador nao interfiram
+    }
 }
