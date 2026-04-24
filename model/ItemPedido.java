8194b63ff7c132d2fc294eba4eb139a5e56887c3
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2026-04-24 14:19:17 -0300

    Add Getters e Setters classe ItemPedido

--- model/ItemPedido.java
+++ model/ItemPedido.java
@@ -1,9 +1,23 @@
-
 package model;
 
 public class ItemPedido {
 
 	private Produto produto;
 	private int quantidade;
+	
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
 	
 }
