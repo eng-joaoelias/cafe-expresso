20e8cc806ad3b8d9318716aa147345cfdc362f12
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2026-04-24 14:14:18 -0300

    Envio da classe Produto
    RF01: O sistema deve permitir o cadastro de produtos com nome e preço.

---
+++ model/Produto.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Produto {
+	private String nome;
+	private double precounitario;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getPrecounitario() {
+		return precounitario;
+	}
+	public void setPrecounitario(double precounitario) {
+		this.precounitario = precounitario;
+	}
+	
+	
+}
