463bb7b65bbdf9a78ee287bd40aff2f13d9552b7
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2026-04-24 15:23:23 -0300

    Imports adicionados no teste de Pedidos.

--- model/PedidoTest.java
+++ model/PedidoTest.java
@@ -1,1 +1,10 @@
+package model;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class PedidoTest {
+	
+}
