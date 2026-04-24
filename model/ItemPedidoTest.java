6b077bf45afd752eace8ef30e25849c1f4b28539
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2026-04-24 14:42:31 -0300

    Cria item de pedido com produto e qtd validos.

--- model/ItemPedidoTest.java
+++ model/ItemPedidoTest.java
@@ -14,4 +14,16 @@ class ItemPedidoTest {
         // Preparação: Criamos um produto que será usado nos testes do Item
         produtoPadrao = new Produto("Teclado Mecânico", 250.00);
     }
+
+    @Test
+    @DisplayName("Deve criar um item de pedido com produto e quantidade válidos")
+    void deveCriarItemPedidoComSucesso() {
+        ItemPedido item = new ItemPedido(produtoPadrao, 3);
+        
+        assertAll("Verificação de atributos do item",
+            () -> assertEquals(produtoPadrao, item.getProduto(), "O produto deve ser o mesmo que foi passado"),
+            () -> assertEquals(3, item.getQuantidade(), "A quantidade deve ser 3"),
+            () -> assertEquals("Teclado Mecânico", item.getProduto().getNome(), "O nome do produto dentro do item deve estar correto")
+        );
+    }
 }
